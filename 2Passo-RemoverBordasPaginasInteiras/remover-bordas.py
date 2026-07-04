4e93fb12d25d3c8dc3d1e932b56ce25e93693e87
matheus <matheus.lachouski.cavalcante@escola.pr.gov.br>
2026-07-04 16:10:49 -0300

    dividi ao meio

--- 2Passo-RemoverBordasPaginasInteiras/remover-bordas.py
+++ 2Passo-RemoverBordasPaginasInteiras/remover-bordas.py
@@ -30,7 +30,7 @@ for nome_arquivo in os.listdir(pasta_imagens):
 
         largura, altura = imagem.size
 
-        caixa_corte = (215, 387, largura - 63, altura - 191) # ATUALIZE AQUI OS VALORES DE CORTE (esquerda, superior, direita, inferior)
+        caixa_corte = (260, 387, largura - 216, altura - 191) # ATUALIZE AQUI OS VALORES DE CORTE (esquerda, superior, direita, inferior)
         imagem_cortada = imagem.crop(caixa_corte)
 
         caminho_saida = os.path.join(pasta_saida, nome_arquivo)
